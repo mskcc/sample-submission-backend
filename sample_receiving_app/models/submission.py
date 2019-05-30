288231c5920a49a25c58a7e73934f4716e5d3b40
Lisa Wagner <wagnerl@mskcc.org>
2019-05-30 13:25:16 -0400

    index on sub username+request, trm timezone

--- sample_receiving_app/models/submission.py
+++ sample_receiving_app/models/submission.py
@@ -13,17 +13,17 @@ class Submission(db.Model):
     """
 
     __tablename__ = 'submissions'
+    __table_args__ = (db.UniqueConstraint('request_id', 'username', name='req_user'),)
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    request_id = db.Column(db.Integer, nullable=True)
+    username = db.Column(db.String(40), db.ForeignKey('users.username'))
+    request_id = db.Column(db.String(40), nullable=True)
     form_values = db.Column(db.Text(), nullable=True)
     grid_values = db.Column(db.Text(), nullable=True)
     version = db.Column(db.Float(), nullable=True)
     submitted = db.Column(db.Boolean(), nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
     submitted_on = db.Column(db.DateTime, nullable=True)
-    
 
     def add(self):
         db.session.add(self)
@@ -31,17 +31,17 @@ class Submission(db.Model):
 
     def __init__(
         self,
-        user_id,
+        username,
         version,
-        request_id=randint(100000, 999999),
+        request_id,
         submitted_on='test',
         created_on='test',
         form_values='{}',
         grid_values='{}',
         submitted=False,
     ):
-        now = datetime.datetime.now()
-        self.user_id = user_id
+        now = datetime.datetime.utcnow()
+        self.username = username
         self.request_id = request_id
         self.version = version
         self.grid_values = grid_values
@@ -54,19 +54,19 @@ class Submission(db.Model):
         """Return object data in easily serializable format"""
         return {
             'id': self.id,
-            'user_id': self.user_id,
+            'username': self.username,
             'version': self.version,
             'request_id': self.request_id,
             'form_values': self.form_values,
             'grid_values': self.grid_values,
             'submitted': self.submitted,
-            'created_on': self.created_on,
-            'submitted_on': self.submitted_on,
+            'created_on': self.created_on.strftime('%Y-%m-%d %H:%M:%S'),
+            'submitted_on': self.submitted_on.strftime('%Y-%m-%d %H:%M:%S') if self.submitted_on else None,
         }
 
 
 # def insert_initial_values(*args, **kwargs):
-#     db.session.add(Submission(user_id=1))
+#     db.session.add(Submission(username=1))
 
 #     db.session.commit()
 
